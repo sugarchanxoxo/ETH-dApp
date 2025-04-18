4f3933a459e21e82ae25ca210dcfc766548709f8
Sugar <140928.miina@gmail.com>
2025-04-18 15:02:57 +0900

    section-4 feature completed

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -155,6 +155,10 @@ const App = () => {
         );
         let count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
+        // コントラクトの現在の資金額がConsoleに出力されます
+        const contractBalance = await provider.getBalance(wavePortalContract.address);
+        console.log("Contract balance:", ethers.utils.formatEther(contractBalance));
+
         /* コントラクトに「wave」を書き込む */
         const waveTxn = await wavePortalContract.wave(messageValue, {
           gasLimit: 300000,
@@ -164,6 +168,20 @@ const App = () => {
         console.log("Mined -- ", waveTxn.hash);
         count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
+        const contractBalancePost = await provider.getBalance(
+          wavePortalContract.address
+        );
+        /* コントラクトの残高が減っていることを確認 */
+        if (contractBalancePost.lt(contractBalance)) {
+          /* 減っていたら下記を出力 */
+          console.log("User won ETH!");
+        } else {
+          console.log("User didn't win ETH.");
+        }
+        console.log(
+          "Contract balance after wave:",
+          ethers.utils.formatEther(contractBalancePost)
+        );
       } else {
         console.log("Ethereum object doesn't exist!");
       }
